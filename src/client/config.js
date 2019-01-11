fdfffe29685441978efd58701bcca42366f9437f
Markus Lerner <m@markuslerner.com>
2019-01-11 12:34:51 +0000

    Map data added

--- src/client/config.js
+++ src/client/config.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import Detector from './three/Detector';
 
-const mapVersion = '4.1.0.1';
+const mapVersion = '4.1.0.2';
 
 
 
@@ -15,15 +15,15 @@ const config = {
 
   rendererContainer: '#container',
 
-  mapDataFile: CDN_URL + 'data/map/4.1.0/country_data.json?v=' + mapVersion,
+  mapDataFile: CDN_URL + 'map/4.1.0/country_data.json?v=' + mapVersion,
 
   visaRequirementsFile: CDN_URL + VISA_REQUIREMENTS_URL,
 
   mergeDataFromMapDataFile2: false,
   mergeDataFromDisputedAreasFile: false,
-  // mapDataFile: CDN_URL + 'data/map/4.1.0/ne_50m_admin_0_countries_simplified.json?v=' + mapVersion,
-  mapDataFile2: CDN_URL + 'data/map/4.1.0/ne_10m_admin_0_countries_simplified.json?v=' + mapVersion, // merge into: ne_50m_admin_0_countries_simplified
-  disputedAreasFile: CDN_URL + 'data/map/4.1.0/ne_10m_admin_0_disputed_areas_simplified.json?v=' + mapVersion,
+  // mapDataFile: CDN_URL + 'map/4.1.0/ne_50m_admin_0_countries_simplified.json?v=' + mapVersion,
+  mapDataFile2: CDN_URL + 'map/4.1.0/ne_10m_admin_0_countries_simplified.json?v=' + mapVersion, // merge into: ne_50m_admin_0_countries_simplified
+  disputedAreasFile: CDN_URL + 'map/4.1.0/ne_10m_admin_0_disputed_areas_simplified.json?v=' + mapVersion,
 
   saveMapData: false,
   mergedCountriesFilename: 'country_data.json',
