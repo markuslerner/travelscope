959b806ac1d94e4bd9bb9351781188fa25b13739
Markus Lerner <m@markuslerner.com>
2020-04-30 11:47:33 +0200

    Replace author logo with author support button

--- public/index.php
+++ public/index.php
@@ -180,11 +180,8 @@
 
 				<li><a href="#" id="button_disclaimer">Disclaimer</a></li>
 
-				<li id="logo_ml">
-					<a href="https://www.markuslerner.com/" target="_blank">
-						<span class="text">Concept/development:</span>
-						<img alt="Markus Lerner Design" src="assets/img/ml-white.svg"/>
-					</a>
+        <li id="support">
+          <style>.bmc-button-n img{height: 34px !important;width: 35px !important;margin-bottom: 1px !important;box-shadow: none !important;border: none !important;vertical-align: middle !important;}.bmc-button-n{ left: 15px; transform-origin: center left; transform: scale(0.7, 0.7); padding: 7px 10px 7px 10px !important;line-height: 35px !important;height:51px !important;min-width:217px !important;text-decoration: none !important;display:inline-flex !important;color:#FFFFFF !important;background-color:#FF813F !important;border-radius: 5px !important;border: 1px solid transparent !important;padding: 7px 10px 7px 10px !important;font-size: 28px !important;letter-spacing:0.6px !important; margin: 0 auto !important;font-family:'Cookie', cursive !important;-webkit-box-sizing: border-box !important;box-sizing: border-box !important;}.bmc-button-n:hover, .bmc-button-n:active, .bmc-button-n:focus {text-decoration: none !important; opacity: 0.85 !important;color:#FFFFFF !important;}</style><link href="https://fonts.googleapis.com/css?family=Cookie" rel="stylesheet"><a class="bmc-button-n" target="_blank" href="https://www.buymeacoffee.com/markuslerner"><img src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg" alt="Buy me a coffee"><span style="margin-left:15px;font-size:28px !important;">Buy me a coffee</span></a>
 				</li>
 			</ul>
 
