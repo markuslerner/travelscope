c5d8cd14c6cea99d8daafb7714627a5c4f2628bc
Markus Lerner <m@markuslerner.com>
2022-10-02 14:36:53 +0200

    Add another alternative start string

--- public/php/inc/parse_wikitext.inc.php
+++ public/php/inc/parse_wikitext.inc.php
@@ -62,6 +62,11 @@ function parseWikiText($text, $debug, $country_name) {
 		$rows = getAllRows($text, $start_string);
 	}
 
+	if(sizeof($rows) == 0) {
+		$start_string = "class=\"wikitable sortable mw-collapsible mw-collapsed\"";
+		$rows = getAllRows($text, $start_string);
+	}
+
 	// load info for territories (not used yet, because many pages still don't have this info in tabular form):
 	// $start_string = "{| class=\"wikitable\"";
 	// $rows2 = getAllRows($text, $start_string);
