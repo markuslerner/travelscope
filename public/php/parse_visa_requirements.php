916cc1270cbc34b5d44ff48814cc96c004906121
Markus Lerner <m@markuslerner.com>
2024-04-01 16:09:31 +0200

    Reformat

--- public/php/parse_visa_requirements.php
+++ public/php/parse_visa_requirements.php
@@ -5,9 +5,9 @@ require __DIR__ . '/../../vendor/autoload.php';
 $dotenv = new Dotenv\Dotenv(__DIR__ . '/../../');
 $dotenv->load();
 
-if(!isset($_GET['n']) || $_GET['n'] !== getenv('SCRIPT_NONCE')) {
-  http_response_code(403);
-  die('Access not allowed');
+if (!isset($_GET['n']) || $_GET['n'] !== getenv('SCRIPT_NONCE')) {
+    http_response_code(403);
+    die('Access not allowed');
 }
 
 // settings:
@@ -22,18 +22,23 @@ $load_from_cache = true; // true, else load from wikipedia, unless forced later
 $debug = false; // false
 $debug_print_json = false;
 
+if (!file_exists($data_folder)) {
+    mkdir($data_folder);
+}
 
+if (!file_exists($wikipedia_cache_folder)) {
+    mkdir($wikipedia_cache_folder);
+}
 
-if(!file_exists($data_folder)) mkdir($data_folder);
-if(!file_exists($wikipedia_cache_folder)) mkdir($wikipedia_cache_folder);
-if(!file_exists($json_archive_folder)) mkdir($json_archive_folder);
+if (!file_exists($json_archive_folder)) {
+    mkdir($json_archive_folder);
+}
 
-require_once('class.XMLParser.php');
-require_once('inc/parse_wikitext.inc.php');
-require_once('inc/countries.inc.php');
+require_once 'class.XMLParser.php';
+require_once 'inc/parse_wikitext.inc.php';
+require_once 'inc/countries.inc.php';
 
 echo sizeof($countries) . " Wikipedia pages in list.<br/>\n";
-
 
 $parser = new XMLParser();
 $parser->debug = false;
@@ -56,62 +61,64 @@ $wikipedia_cache_filename = $wikipedia_cache_folder . "/" . $year . "-" . $month
 // $wikipedia_cache_filename = $wikipedia_cache_folder . "/" . $year . "-" . $month . "-" . $day . "_Visa_requirements_by_nationality.xml";
 
 // force archive update, if current archive file doesn't exist:
-if(!file_exists($wikipedia_cache_filename)) {
-	$load_from_cache = false;
+if (!file_exists($wikipedia_cache_filename)) {
+    $load_from_cache = false;
 }
 
-if(file_exists($wikipedia_cache_filename) && $load_from_cache) {
-  echo "Loading data from cache file '" . $wikipedia_cache_filename . "'<br/>\n";
+if (file_exists($wikipedia_cache_filename) && $load_from_cache) {
+    echo "Loading data from cache file '" . $wikipedia_cache_filename . "'<br/>\n";
 
-	$export_xml_string = file_get_contents($wikipedia_cache_filename);
+    $export_xml_string = file_get_contents($wikipedia_cache_filename);
 
 } else {
-	$doc = new DOMDocument('1.0');
-	$doc->formatOutput = true;
+    $doc = new DOMDocument('1.0');
+    $doc->formatOutput = true;
 
-	$root = $doc->createElement('pages');
-	$root = $doc->appendChild($root);
+    $root = $doc->createElement('pages');
+    $root = $doc->appendChild($root);
 
-	$created = $doc->createAttribute('created');
-	$created->value = date("c", $time);
-	$root->appendChild($created);
+    $created = $doc->createAttribute('created');
+    $created->value = date("c", $time);
+    $root->appendChild($created);
 
-	foreach ($countries as $country) {
-		$pagename = $country[2];
-		$export_url = $wikipedia_export_url . urlencode($pagename);
-		if($debug) echo "Exporting pages from Wikipedia: " . $export_url . "<br/><br/>\n\n";
+    foreach ($countries as $country) {
+        $pagename = $country[2];
+        $export_url = $wikipedia_export_url . urlencode($pagename);
+        if ($debug) {
+            echo "Exporting pages from Wikipedia: " . $export_url . "<br/><br/>\n\n";
+        }
 
-		if(!$xml = file_get_contents($export_url)) {
-			echo "Error loading pages from Wikipedia";
-		}
+        if (!$xml = file_get_contents($export_url)) {
+            echo "Error loading pages from Wikipedia";
+        }
 
-		$parser->loadXML($xml);
-		$pages = $parser->doc->getElementsByTagName('page');
-		foreach ($pages as $page) {
-			// Import the node, and all its children, to the document
-			$page = $doc->importNode($page, true);
-			// And then append it to the "<root>" node
-			$root->appendChild($page);
-		}
+        $parser->loadXML($xml);
+        $pages = $parser->doc->getElementsByTagName('page');
+        foreach ($pages as $page) {
+            // Import the node, and all its children, to the document
+            $page = $doc->importNode($page, true);
+            // And then append it to the "<root>" node
+            $root->appendChild($page);
+        }
 
-		// duplicates check:
-		$count = 0;
-		foreach ($countries as $country2) {
-			if($country[0] == $country2[0]) {
-				$count++;
-			}
-		}
-		if($count > 1) {
-		   echo "Error: duplicate found in countries list: " . $country[0] . " ";
-		}
+        // duplicates check:
+        $count = 0;
+        foreach ($countries as $country2) {
+            if ($country[0] == $country2[0]) {
+                $count++;
+            }
+        }
+        if ($count > 1) {
+            echo "Error: duplicate found in countries list: " . $country[0] . " ";
+        }
 
-	}
-	$export_xml_string = $doc->saveXML();
+    }
+    $export_xml_string = $doc->saveXML();
 
-	if(!file_exists($wikipedia_cache_filename)) {
-		file_put_contents($wikipedia_cache_filename, $export_xml_string);
-		echo "<br><b>Wikipedia cache XML file saved to: '" . $wikipedia_cache_filename . "'.</b><br/>\n\n";
-	}
+    if (!file_exists($wikipedia_cache_filename)) {
+        file_put_contents($wikipedia_cache_filename, $export_xml_string);
+        echo "<br><b>Wikipedia cache XML file saved to: '" . $wikipedia_cache_filename . "'.</b><br/>\n\n";
+    }
 
 }
 
@@ -121,67 +128,63 @@ $parser->loadXML($export_xml_string);
 
 $countries_json = array();
 
-require_once('inc/parse_wikipedia_export_xml.inc.php');
+require_once 'inc/parse_wikipedia_export_xml.inc.php';
 
 echo "<b>Visa requirements loaded for " . sizeof($countries_json) . " countries.</b><br>\n";
 
 foreach ($countries_json as $key => $country_json) {
-	$json_contents .= $country_json;
-   	if($key < sizeof($countries_json) - 1) {
-		$json_contents .= ",\n\n";
-	}
+    $json_contents .= $country_json;
+    if ($key < sizeof($countries_json) - 1) {
+        $json_contents .= ",\n\n";
+    }
 }
 
 $json_contents .= "\t]\n}";
 
-
-if($write_json_file) {
-	$json_archivefilename = $json_archive_folder . "/" . $year . "-" . $month . "-" . $day . "_" . $hour . "." . $minute . $json_filename;
-	if(!file_exists($json_archivefilename)) {
-		file_put_contents($json_archivefilename, $json_contents);
-		echo "<b>Archive JSON file saved to: '" . $json_archivefilename . "'.</b><br/>\n";
-	}
-
-}
-if($debug_print_json) {
-	header('Content-Type: application/json');
-	echo $json_contents;
-}
-
-
-function getSubString($str, $start_string, $end_string) {
-	$start = stripos($str, $start_string) + strlen($start_string);
-	return substr($str, $start, stripos($str, $end_string) - $start);
-}
-
-
-function getSubStringAfter($str, $start_string) {
-	$start = stripos($str, $start_string) + strlen($start_string);
-	return substr($str, $start);
-}
-
-
-function cleanURLs($url) {
-  $U = explode(' ',$url);
-
-  $W =array();
-  foreach ($U as $k => $u) {
-    if (stristr($u,'http') || (count(explode('.',$u)) > 1)) {
-      unset($U[$k]);
-      return cleanURLs( implode(' ',$U));
+if ($write_json_file) {
+    $json_archivefilename = $json_archive_folder . "/" . $year . "-" . $month . "-" . $day . "_" . $hour . "." . $minute . $json_filename;
+    if (!file_exists($json_archivefilename)) {
+        file_put_contents($json_archivefilename, $json_contents);
+        echo "<b>Archive JSON file saved to: '" . $json_archivefilename . "'.</b><br/>\n";
     }
-  }
-  return implode(' ',$U);
+
+}
+if ($debug_print_json) {
+    header('Content-Type: application/json');
+    echo $json_contents;
 }
 
-
-function getCountryByTitle($countries, $title) {
-	foreach ($countries as $country) {
-   		if($country[2] == str_replace(" ", "_", $title)) {
-   			return $country;
-   		}
-	}
+function getSubString($str, $start_string, $end_string)
+{
+    $start = stripos($str, $start_string) + strlen($start_string);
+    return substr($str, $start, stripos($str, $end_string) - $start);
 }
 
+function getSubStringAfter($str, $start_string)
+{
+    $start = stripos($str, $start_string) + strlen($start_string);
+    return substr($str, $start);
+}
 
-?>
+function cleanURLs($url)
+{
+    $U = explode(' ', $url);
+
+    $W = array();
+    foreach ($U as $k => $u) {
+        if (stristr($u, 'http') || (count(explode('.', $u)) > 1)) {
+            unset($U[$k]);
+            return cleanURLs(implode(' ', $U));
+        }
+    }
+    return implode(' ', $U);
+}
+
+function getCountryByTitle($countries, $title)
+{
+    foreach ($countries as $country) {
+        if ($country[2] == str_replace(" ", "_", $title)) {
+            return $country;
+        }
+    }
+}
